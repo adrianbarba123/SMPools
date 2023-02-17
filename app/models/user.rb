ad75b62e3949d15dff759fcb63ea7b20d6017aa9
Adrian Barba <adrianbarba123@gmail.com>
2023-02-16 18:36:51 -0800

    added email validator gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
-class User < ApplicationRecord
+class User < ApplicationRecord::Base
+    has_secure_password
+
+    validates :name, presence: true, length: { in: 2..70 }
+    validates :email,  presence: true, uniqueness: true
+    validates :password, length: { in: 2..20 }
 end
