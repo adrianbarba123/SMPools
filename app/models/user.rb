97532015649a0957dda9b25d9cbe1047e41fe523
Adrian Barba <adrianbarba123@gmail.com>
2023-02-16 18:37:52 -0800

    implemented email validation with the email_validator gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord::Base
     has_secure_password
 
     validates :name, presence: true, length: { in: 2..70 }
-    validates :email,  presence: true, uniqueness: true
+    validates :email, email: true #no more validations because of email_validator gem
     validates :password, length: { in: 2..20 }
 end
